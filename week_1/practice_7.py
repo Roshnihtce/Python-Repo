2e31b4bc21bd83a09d30a800a063bb1d84889a08
= <=>
2024-07-15 18:00:12 +0530

    practice_7

---
+++ week_1/practice_7.py
@@ -0,0 +1,8 @@
+odd_numbers={1,3,5,7,9,11}
+even_numbers={2,4,6,8,10,12}
+union=odd_numbers.union(even_numbers)
+intersection=odd_numbers.intersection(even_numbers)
+print("A set of the first five odd numbers:",odd_numbers)
+print("A set of the first five even numbers:",even_numbers)
+print("Union of these sets:",union)
+print("Intersection of these sets:",intersection)
