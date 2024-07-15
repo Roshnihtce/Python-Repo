260f8852a29e653ae2bbb8ac8772611da0a5b87d
= <=>
2024-07-15 18:00:43 +0530

    practice_3

---
+++ week_1/practice_3.py
@@ -0,0 +1,8 @@
+odd_num=[x for x in range(1,11)]
+print(odd_num)
+square_num=[num**2 for num  in odd_num]
+print(square_num)
+print("list of first 10 odd number")
+print(odd_num)
+print("list of the square of the odd number")
+print(square_num)
