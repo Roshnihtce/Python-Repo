f692cf8e60faa9c12550b126710bd081131de885
= <=>
2024-07-15 17:59:47 +0530

    practice_6

---
+++ week_1/practice_6.py
@@ -0,0 +1,5 @@
+tuple = ((5,10,15), (20,25,30))
+secondelement_firstnestedtuple = tuple[0][1]
+firstelement_secondnestedtuple = tuple[1][0]
+print(secondelement_firstnestedtuple)  
+print(firstelement_secondnestedtuple)
