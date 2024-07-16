57977685eaee7eb5423934a7b5cb84c103345480
= <=>
2024-07-16 18:04:06 +0530

    practice_8

---
+++ week_1/practice_8.py
@@ -0,0 +1,17 @@
+# 1. Create a set of the first five prime numbers.
+# 2. Add the number 11 to the set.
+# 3. Remove the number 2 from the set.
+# 4. Check if the number 7 is in the set.
+# 5. Print the final set.
+
+prime_numbers={1,2,3,5,7}
+print(prime_numbers.add(11))
+prime_numbers.remove(2)
+check=7 in prime_numbers
+print(check)
+print("final set:",prime_numbers)
+
+
+
+
+
