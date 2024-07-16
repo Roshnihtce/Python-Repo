3debb11cb4730ad57cf0532434aafdb1bae128bf
= <=>
2024-07-16 18:05:30 +0530

    practice_10

---
+++ week_1/practice_10.py
@@ -0,0 +1,14 @@
+# 1. Create a dictionary with three key-value pairs representing a student's name,
+# age, and grades.
+# 2. Update the student's age.
+# 3. Add a new key-value pair for the student's favorite subject.
+# 4. Print the final dictionary.
+
+dictionary={
+     "student's name":"Roshni",
+     "Age":18,
+     "Grade":85.4
+}
+dictionary["Age"]=19
+dictionary["Subject"]="Science"
+print(dictionary)
