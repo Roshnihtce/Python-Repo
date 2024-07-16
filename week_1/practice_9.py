3be8b9d015a69ac3111d380a17bd7825f7f55d19
= <=>
2024-07-16 18:04:33 +0530

    practice_9

---
+++ week_1/practice_9.py
@@ -0,0 +1,14 @@
+# 1. Create a set of vowels.
+# 2. Check if the letter 'e' is in the set.
+# 3. Check if the letter 'x' is in the set.
+# 4. Print the results.
+
+vowel_set={"a","e","i","o","u"}
+print('e' in vowel_set)
+print('x'in vowel_set)
+print(vowel_set)
+
+
+
+        
+
